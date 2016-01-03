28cdb25e696cc6352f87f1d54132dd15ea788f2b
farzaan <farzaan.nasar@gmail.com>
2016-01-02 18:01:06 -0600

    Using commit script

--- java/helloworld/src/helloworld/main.java
+++ java/helloworld/src/helloworld/main.java
@@ -4,7 +4,16 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("hello world, WTH");
-
+		
+		
+		String text = "iwe";
+		
+		switch(text){
+		case "iwe":
+			System.out.println("yo kai watch");
+			break;
+		default:
+			System.out.println("This");
+		}
 	}
-
 }
