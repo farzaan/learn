1a248d9213373662ff23f005a82ea43e7667bd27
farzaan <farzaannasar@Syeds-MacBook-Pro.local>
2016-05-27 11:36:21 -0500

    not using commit script

---
+++ java/StartOfJava/src/printing/Printer.java
@@ -0,0 +1,10 @@
+package printing;
+
+/**
+ * Created by farzaannasar on 5/27/16.
+ */
+public class Printer
+{
+    public boolean isOn;
+    public String modelnumber;
+}
---
+++ java/StartOfJava/src/testClass.java
@@ -0,0 +1,9 @@
+/**
+ * Created by farzaannasar on 5/27/16.
+ */
+public class testClass {
+    public static void man(String[] args)
+    {
+        Printer p = new Printer();
+    }
+}
