6be3e61b8e077cfb441a807a96e231360851b4d6
farzaan <farzaannasar@Syeds-MacBook-Pro.local>
2016-06-12 12:10:39 -0500

    Using Commit script

--- java-coursera/java-coursera/tagfinder.java
+++ java-coursera/java-coursera/tagfinder.java
@@ -1,15 +1,30 @@
-
+import edu.duke.*;
+import java.io.*;
 
 public class tagfinder 
 {
     public String findProtein(String dna){
         int start = dna.indexOf("atg");
         int stop = dna.indexOf("tag", start+3);
+        if(start == -1){
+            return ("");
+        }
         if ((stop-start) % 3 == 0){
-            return dna.substring(start, stop+3);
+            return dna.substring(start+3, stop);
         }
         else{
             return ("");
         }
     }
+    public void testing(){
+    String input = ("gatatgggctgctagtgtggtagtgatgctgctcgctgctgatccgtcgtaagtgtgtgtacgatgctagc");
+    String correct = ("ggctgc");
+    String resuly = findProtein(input);
+    if (correct.equals(resuly)) {
+        System.out.println("right for "+ input);
+    }
+    else {
+        System.out.println("wrong for testcase "+ resuly);
+    }
+}
 }
