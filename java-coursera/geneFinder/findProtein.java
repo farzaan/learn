5d1d2461729f7de480ec9cc11e56624c98825b64
farzaan <farzaannasar@Syeds-MacBook-Pro.local>
2016-06-13 15:31:42 -0500

    using commit script

--- java-coursera/geneFinder/findProtein.java
+++ java-coursera/geneFinder/findProtein.java
@@ -1,10 +1,29 @@
-
+import edu.duke.*;
+import java.io.*;
 
 public class findProtein {
     public String finProtein(String dna){
        int start = dna.indexOf("atg");
        String next = dna.substring(start);
-       return(next);
+       int[] list = new int[] {next.indexOf("tag"), next.indexOf("taa"), next.indexOf("tga")};
+       int i = 0;
+       for(int f : list){
+           i++;
+           if(f != -1){
+              break ;
+            }
+           
+        }
+       String endstring;
+       switch(i){
+           case 1:endstring = "tag";
+                  break;
+           case 2:endstring = "taa";
+                  break;
+           case 3:endstring = "tga";
+                  break;    
+        }
+        return endstring;
     }
     
     public void testing(){
