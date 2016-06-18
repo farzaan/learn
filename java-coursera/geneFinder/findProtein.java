4debbb8621c4e2c07b10d78b1970b21656b4ad77
farzaan <farzaannasar@Syeds-MacBook-Pro.local>
2016-06-18 13:55:56 -0500

    using commit script

--- java-coursera/geneFinder/findProtein.java
+++ java-coursera/geneFinder/findProtein.java
@@ -2,16 +2,23 @@ import edu.duke.*;
 import java.io.*;
 
 public class findProtein {
+    
+    static final String ST_CODON = "atg";
+    
     public String finProtein(String dna){
+       int CODONLEN = 3;
        String nedna = dna.toLowerCase();
-       int start = nedna.indexOf("atg");
+       int start = nedna.indexOf(ST_CODON);
        int end = nedna.indexOf(stopCodon(nedna));
-       return nedna.substring(start, end +3);
+       
+       return nedna.substring(start, end + CODONLEN);
+       
     }
+    
     public String stopCodon(String dna){
        
-        String nedna = dna.toLowerCase();
-       int start = nedna.indexOf("atg");
+       String nedna = dna.toLowerCase();
+       int start = nedna.indexOf(ST_CODON);
        if(start == -1){
           return ""; 
         }
@@ -20,10 +27,13 @@ public class findProtein {
        String next = nedna.substring(start);
        int[] list = new int[] {next.indexOf("tag"), next.indexOf("taa"), next.indexOf("tga")};
        String[] ends = new String[] {"tag", "taa", "tga"};
-       int i = 0;;
+       int i = 0;
+
        String endstring = new String();
+       
        for(int f : list){
            i++;
+       
            if(f != -1){
                endstring = ends[i];
                break;
@@ -33,19 +43,23 @@ public class findProtein {
         
       return endstring;
     }
+    
     public void testing(){
-    String input = ("AATGCTAGTTTAAATCTGA");
-    String nes = finProtein(input);
-    String resuly = stopCodon(input);
-   System.out.println(resuly);
-    System.out.println(nes);
-}
+        String input = ("AATGCTAGTTTAAATCTGA");
+        String nes = finProtein(input);
+        
+        String resuly = stopCodon(input);
+        System.out.println(resuly);
+        System.out.println(nes);
+    }
+
     public void realTesting(){
-        DirectoryResource dr = new DirectoryResource();
+       DirectoryResource dr = new DirectoryResource();
+        
        for(File f : dr.selectedFiles()){
            FileResource fil = new FileResource(f);
            String s = fil.asString();
-           System.out.println("this" + finProtein(s));
+           System.out.println("this is a protein => " + finProtein(s));
         }
     }
 
