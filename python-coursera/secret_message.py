f7270d5d8b645f5628bf22d4fe3bb418984350f0
farzaan <farzaan.nasar@gmail.com>
2017-01-28 19:09:07 -0600

    Added via script.

--- python-coursera/secret_message.py
+++ python-coursera/secret_message.py
@@ -1,12 +1,16 @@
 import os
 def rename_thy_files():
 	#makes list of file name
-	path = "/home/farzaan/Downloads"
+	path = r"/home/farzaan/Downloads/prank"
 	fileList = os.listdir(path)
+
+	print path
 	#rename them nao
 	for file in fileList:
 		newFilename = file.translate(None, "0123456789")
 		print file, newFilename
-		os.rename(path + "/" + file, newFilename)
-	return file
-print(rename_thy_files())
+		os.rename(path + "/" + file, path + "/" + newFilename)
+
+	return 
+
+rename_thy_files()
