9406f6ec5299342d79d91f57eb5245fa646d0929
farzaan <farzaan.nasar@gmail.com>
2017-03-27 11:37:37 -0500

    Added via script.

--- python-coursera/mindstorms.py
+++ python-coursera/mindstorms.py
@@ -1,10 +1,17 @@
-
 import turtle
 
 def makeasquare():
 	window = turtle.Screen()
-	
+	window.bgcolor("red")
 	brad = turtle.Turtle()
-	brad.forward(100)
+	brad.color("pink")
+	brad.shape("turtle")
+	i = 4
+	while (i > 0):
+		brad.forward(100)
+		brad.right(90)
+		i -= 1
+	
+	
 	window.exitonclick()
 makeasquare() 
