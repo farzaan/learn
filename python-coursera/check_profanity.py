f7ac050c0edac28c9ff8ba5fbdde0b7696b48063
farzaan <farzaan.nasar@gmail.com>
2017-06-01 12:12:00 -0500

    Added via script.

--- python-coursera/check_profanity.py
+++ python-coursera/check_profanity.py
@@ -1,9 +1,18 @@
+import urllib.request
+import urllib.parse
+import urllib
+	
 def read_text():
 	quotes = open("D:\FARZAAN\dev\ws\mygithub\learn\python-coursera\houston.txt")
 	contentOfFile = quotes.read()
 	print(contentOfFile)
-	print("rte")
+	check_profanity(contentOfFile)
+
 	#close afteryou open
 	quotes.close()
-
+def check_profanity(text_to_check):
+	host = urllib.parse.quote(text_to_check)
+	connection = urllib.request.urlopen("http://www.wdylike.appspot.com/?q=" + host)
+	output = connection.read()
+	print(output)
 read_text()
