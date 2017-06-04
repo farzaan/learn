31ec6773064a0fbc1353b3a6fadee17c4a25d065
farzaan <farzaan.nasar@gmail.com>
2017-06-04 11:00:57 -0500

    Added via script.

---
+++ fullstack/movies_project/entertainment_center.py
@@ -0,0 +1,28 @@
+import media
+import fresh_tomatoes
+
+
+# Creating the movie intances
+toy_story = media.Movie("Toy Story", "A story of a young boy and his toys that come to life", \
+ "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=4KPTXpQehio")
+
+avatar= media.Movie("Avatar", "A story about a marine on a planet", \
+ "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg", "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+inception = media.Movie("Inception", "A story of a young boy and a mean world", \
+ "https://en.wikipedia.org/wiki/File:Inception_(2010)_theatrical_poster.jpg", "https://www.youtube.com/watch?v=YoHD9XEInc0")
+
+forrest_gump = media.Movie("Forrest Gump", "A story of a young boy and his adventures in life", \
+ "https://en.wikipedia.org/wiki/File:Forrest_Gump_poster.jpg", "https://www.youtube.com/watch?v=bLvqoHBptjg")
+
+
+# Adding the movie intances to the movies list
+movies = []
+
+movies.append(toy_story)
+movies.append(avatar)
+movies.append(inception)
+movies.append(forrest_gump)
+
+# Lets display the movies page
+fresh_tomatoes.open_movies_page(movies)
---
+++ fullstack/movies_project/media.py
@@ -0,0 +1,30 @@
+import webbrowser
+
+
+
+class Movie():
+	"""
+
+	
+    Constructer for several instances of movie
+    Note:
+        Do not include the `self` parameter in the ``Args`` section.
+
+    Args:
+        movie_title(str): title of movie
+        movie_storyline(str): movie stroyline
+        poster_image(str): url for poster image 
+        trailer_youtube(str): url for yt trailer
+        
+    """
+	
+	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+		self.title = movie_title
+		self.storyline = movie_storyline
+		self.poster_image_url = poster_image
+		self.trailer_youtube_url = trailer_youtube
+	
+
+
+	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url)
