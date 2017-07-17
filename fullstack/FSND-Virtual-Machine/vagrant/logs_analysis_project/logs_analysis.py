73d98c3bb888a5191c135a1a4e2bc3f25a8974da
farzaan <farzaan.nasar@gmail.com>
2017-07-17 17:17:54 -0500

    Added via script.

--- fullstack/FSND-Virtual-Machine/vagrant/logs_analysis_project/logs_analysis.py
+++ fullstack/FSND-Virtual-Machine/vagrant/logs_analysis_project/logs_analysis.py
@@ -1,9 +1,26 @@
 import psycopg2
 
+import csv
+
 DBNAME = "news"
 
 def top_articles():
     DB = psycopg2.connect(database = DBNAME)
-    c = db.cursor()
-    c.execute("select * from authors")
-    
+    c = DB.cursor()
+    c.execute("select a.title, count(a.title) as cnt from articles as a, log as l where a.slug = replace(l.path, '/article/', '') group by a.title order by cnt desc limit 3")
+    posts = c.fetchall()
+    DB.close()
+    return posts  
+
+if __name__ == '__main__':
+
+
+
+    data = top_articles()
+
+    with open('view_report.txt','wb') as out:
+        csv_out=csv.writer(out)
+        csv_out.writerow(['TITLE','VIEWS'])
+        for row in data:
+            csv_out.writerow(row)    
+       
