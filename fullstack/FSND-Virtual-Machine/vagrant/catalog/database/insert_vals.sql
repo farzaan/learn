cdcd10f0de52a3a4123b6cb061e3a76e00669f65
farzaan <farzaan.nasar@gmail.com>
2017-08-04 16:26:57 -0500

    Added via script.

---
+++ fullstack/FSND-Virtual-Machine/vagrant/catalog/database/insert_vals.sql
@@ -0,0 +1,8 @@
+delete from item where name like 'Basketball%';
+
+insert into item (name, description, category_id) values('Basketball', 'Ball for playing ball', 3);
+insert into item (name, description, category_id) values('Basketball-2', 'Ball for playing ball', 3);
+insert into item (name, description, category_id) values('Basketball-3', 'Ball for playing ball', 3);
+
+
+select * from item;
