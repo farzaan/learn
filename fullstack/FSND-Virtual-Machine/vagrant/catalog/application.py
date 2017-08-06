9bca99e01e03f7c537cb9d7f09a88eb093bcadf6
farzaan <farzaan.nasar@gmail.com>
2017-08-06 13:53:09 -0500

    Added via script.

--- fullstack/FSND-Virtual-Machine/vagrant/catalog/application.py
+++ fullstack/FSND-Virtual-Machine/vagrant/catalog/application.py
@@ -39,7 +39,8 @@ session = DBSession()
 @app.route("/catalog/")
 def showCatalog():  
     categories = session.query(Category).order_by(asc(Category.name))
-    return render_template('catalog.html', categories=categories)
+    items = session.query(Item).order_by(asc(Item.id)).limit(10)
+    return render_template('catalog.html', categories=categories, items=items)
 
 @app.route("/catalog/<string:cat_name>/items")
 def showCategory(cat_name):
@@ -59,6 +60,31 @@ def showItem(category_name, item_name):
 
     return render_template('item.html', category=category, item=item)
 
+@app.route("/catalog/<string:category_name>/<string:item_name>/edit", methods=['GET', 'POST'])
+def editItem(category_name, item_name):
+    category = session.query(Category).filter_by(name=category_name).one()
+    categories = session.query(Category).order_by(asc(Category.name))
+    item = session.query(Item).filter_by(name=item_name).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            item.name = request.form['name']
+        if request.form['description']:
+            item.description = request.form['description']
+        if request.form:
+            item.category_name = request.form['category']
+        return redirect(url_for('showItem', category_name=category_name, item_name=item_name))
+    else:
+        return render_template('editItem.html', categories=categories, item=item, category=category) 
+@app.route("/catalog/<string:category_name>/<string:item_name>/delete", methods=['GET', 'POST'])
+def deleteItem(category_name, item_name):
+    category = session.query(Category).filter_by(name=category_name).one()
+    item = session.query(Item).filter_by(name=item_name).one()
+    if request.method == 'POST':
+        session.delete(item)
+        session.commit()
+        return redirect(url_for('showItem', category_name=category_name, item_name=item_name))
+    else:
+        return render_template('deleteItem.html', item=item, category=category)      
 
 #@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit',methods=['GET', 'POST'])
 #def editItem()
@@ -71,6 +97,8 @@ if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
             
     '''
+    <!---->
+    <a href = "{{url_for('showItem', category_name=category.name, item_name=i.name)}}">
     clientid: 168375451406-pmikr862nn05e4254vbbs4rhrqa1neft.apps.googleusercontent.com
     clientsecretion: Tbx2_r-RgGrF_0wzQJQLGHNc
     '''    
